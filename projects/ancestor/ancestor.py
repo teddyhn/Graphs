35376b0bf97518b951089a908ed9a64767008784
teddyhn <tngo97@gmail.com>
2020-02-18 11:56:25 -0800

    ancestor.py done

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,15 @@
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+def earliest_ancestor(ancestors, starting_node, visited=None):
+    if visited is None:
+        visited = []
+
+    for k in ancestors:
+        if k[1] == starting_node:
+            visited.append(k[0])
+            return earliest_ancestor(ancestors, k[0], visited)
+    
+    for k in ancestors:
+        if k[0] == starting_node and k[0] in visited:
+            return k[0]
+
+    return -1
